c85c06af486a84d560801c9d572122254f9699eb
Claus L. Henriksen <clih@itu.dk>
2012-02-29 11:22:09 +0100

    added getter

--- src/Union/Point.java
+++ src/Union/Point.java
@@ -3,7 +3,24 @@ package Union;
 import java.math.BigDecimal;
 
 public class Point {
-	private int ID, numberOfConenctions;
+	private int ID, numberOfConnections = 0;
 	private BigDecimal xk, yk;
-	private int connections[][];
+	private double[][] connections;
+	
+	public Point(int id, BigDecimal xk, BigDecimal yk){
+		this.ID = id;
+		this.xk = xk;
+		this.yk = yk;
+		connections = new double[2][2];
+	}
+	
+	public void addConnection(int id, double length){
+		connections[numberOfConnections][1] = new Double(id);
+		connections[numberOfConnections][2] = length;
+		numberOfConnections++;
+	}
+	
+	public double[][] getConnections(){
+		return connections;
+	}
 }
