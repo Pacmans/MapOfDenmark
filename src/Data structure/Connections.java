fa7180dd9d863a36f4d4cd06cec021b308d264ad
Claus L. Henriksen <clih@itu.dk>
2012-03-20 17:16:44 +0100

    working on Connections

--- src/Data structure/Connections.java
+++ src/Data structure/Connections.java
@@ -2,15 +2,36 @@
  * @(#)Connections.java
  *
  *
- * @author 
- * @version 1.00 2012/3/20
+ * @author Pacmans
+ * @version 20. Marts 2012
  */
 
 
 public class Connections {
+  Connection[][] connections;
 
-    public Connections() {
+  public void addConnection(){
+
+  }
+
+  //TODO resizeable + compare, floor, ceiling
+  //TODO add priority constraint
+  /**
+   *  Returns all connections within the given coordinates
+   */
+  public Connection[] getConnections(int x1, int x2, int y1, int y2){
+    Connection[] temp = new Connection[2];
+    int i = floor(x1); //find largest key equal or smaller than x1
+    int j = ceiling(x2); //find smallest key equal or greater than x2
+
+    for(i; i<j; i++){
+      int k = floor(connections[i], y1); //find largest key equal or smaller than y1, within array
+      int l = ceiling(connections[i], y2); //find smallest key equal or greater than y2, within array
+
+      for(k; k<l; k++){
+        temp.add(connections[i][k]);
+      }
     }
-    
-    
+    return temp;
+  }
 }
