f68513e37bc8b73c7b5d1d4c8203d1ebcf29ed58
Claus L. Henriksen <clih@itu.dk>
2012-03-20 13:48:00 +0100

    Added empty Connections and ConnectionsLoader classes

---
+++ src/Data structure/Connections.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)Connections.java
+ *
+ *
+ * @author 
+ * @version 1.00 2012/3/20
+ */
+
+
+public class Connections {
+
+    public Connections() {
+    }
+    
+    
+}
---
+++ src/Data structure/ConnectionsLoader.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)ConnectionsLoader.java
+ *
+ *
+ * @author 
+ * @version 1.00 2012/3/20
+ */
+
+
+public class ConnectionsLoader {
+
+    public ConnectionsLoader() {
+    }
+    
+    
+}
