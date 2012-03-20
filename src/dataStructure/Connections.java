838e00df976212f83f5035910eab4c9b11c9fe9c
Claus L. Henriksen <clih@itu.dk>
2012-03-20 21:28:24 +0100

    added more methods

--- src/dataStructure/Connections.java
+++ src/dataStructure/Connections.java
@@ -10,34 +10,42 @@ package dataStructure;
 
 
 public class Connections {
-  Connection[][] connections;
+  private Connection[][] connections = new Connection[2][2];
+  private int size = 0;
+  
+  //TODO Sort
 
-  public void addConnection(){
-
+  public void addConnections(Connection[] cons){
+     for(int i = 0; i < cons.length; i++){
+       connections[size][i] = cons[i];
+       size++;
+     }
+  }
+  
+  //TODO binsearch this
+  public Connection getConnection(double x1, double y1, double x2, double y2){
+    return null;
   }
 
-  //TODO compare, floor, ceiling
+  //TODO floor, ceiling
   //TODO enlarge coordinate restraints to ensure inclusion of all connections within
   //TODO add priority constraint
   /**
    *  Returns all connections within the given coordinates
    */
-  public Connection[] getConnections(int x1, int x2, int y1, int y2){
+  public Connection[] getConnections(double x1, double x2, double y1, double y2){
     Connection[] temp = new Connection[2];
     int tsize = 0;
     int i = floor(x1); //find largest key equal or smaller than x1
     int j = ceiling(x2); //find smallest key equal or greater than x2
 
-    for(i; i<j; i++){
+    for(i=i; i<j; i++){
       int k = floor(connections[i], y1); //find largest key equal or smaller than y1, within array
       int l = ceiling(connections[i], y2); //find smallest key equal or greater than y2, within array
 
-      connections[10].length = 100;
-      connections[9].length = 50;
-
-      for(k; k<l; k++){
+      for(k=k; k<l; k++){
         if(temp.length == tsize) temp = resize(temp, tsize*2);
-        temp[tsize] = connections[i][k]);
+        temp[tsize] = connections[i][k];
         tsize++;
       }
     }
@@ -61,16 +69,64 @@ public class Connections {
 
   /**
    * Finds floor access in connections[]
+   * Find largest key equal or smaller than
    * @param k
    * @return
    */
   private int floor(double k) {
     //TODO binsearch this
     int x = -1;
-    for(int i = 0; i<connections.length-1; i++){
+    for(int i = 0; i<connections.length; i++){
       if(connections[i][0].getX1() <= k) x = i;
       else break;
     }
     return x;
+  }
+  
+  /**
+   * Find largest key equal or smaller than
+   * @param cons
+   * @param k
+   * @return
+   */
+  private int floor(Connection[] cons, double k){
+  //TODO binsearch this
+    int y = -1;
+    for(int i = 0; i<cons.length; i++){
+      if(cons[i].getY1() <= k) y = i;
+      else break;
+    }
+    return y;
+  }
+  
+  /**
+   * Find smallest key equal or greater than 
+   * @param k
+   * @returns
+   */
+  private int ceiling(double k){
+    //TODO binsearch this
+    int x = -1;
+    for(int i = connections.length; i>=0; i--){
+      if(connections[i][0].getY1() >= k) x = i;
+      else break;
+    }
+    return x;
+  }
+  
+  /**
+   * Find smallest key equal or greater than
+   * @param cons
+   * @param k
+   * @return
+   */
+  private int ceiling(Connection[] cons, double k){
+    //TODO binsearch this
+    int y = -1;
+    for(int i = cons.length; i>=0; i--){
+      if(cons[i].getY1() >= k) y = i;
+      else break;
+    }
+    return y;    
   }
 }
