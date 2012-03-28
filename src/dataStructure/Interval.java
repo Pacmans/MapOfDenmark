a8afd75fbe80c0e6f5990c2dceb52ee86a915971
Claus L. Henriksen <clih@itu.dk>
2012-03-28 12:51:36 +0200

    Interval2D

--- src/dataStructure/Interval.java
+++ src/dataStructure/Interval.java
@@ -9,10 +9,20 @@ public class Interval {
     this.high = high;
   }
   
+  /**
+   * Is x within the interval? 
+   * @param x
+   * @return True if x is within interval
+   */
   public boolean contains(double x){
     return x > low && x < high; 
   }
   
+  /**
+   * Does intervals intersect?
+   * @param b Interval to intersect with this
+   * @return True if intervals intersect
+   */
   public boolean intersects(Interval b){
     if(high < b.getLow()) return false;
     if(b.getHigh() < low) return false;
