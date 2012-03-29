76a28fb15b13e2cb5db7f8264fa931edd16adbb5
bhas <bhas@itu.dk>
2012-03-29 04:56:42 +0300

    Update src/dataStructure/RoadType.java

--- src/dataStructure/RoadType.java
+++ src/dataStructure/RoadType.java
@@ -11,49 +11,49 @@ package dataStructure;
 import java.awt.Color;
 
 public enum RoadType {
-  MOTORVEJ(1, new Color(0,0,0), 1),
-  PROJMOTVEJ(1, new Color(0,0,0), 1),
-  MOTORVEJSTUNNEL(1, new Color(0,0,0), 1),
-  FÆRGE(1, new Color(0,0,0), 1),
-  MOTORTRAFIKVEJ(2, new Color(0,0,0), 1),
-  PROJMOTORTRAFIKVEJ(2, new Color(0,0,0), 1),
-  PROJPRIMÆRVEJ(2, new Color(0,0,0), 1),
-  MOTORVEJSAFKØRSEL(2, new Color(0,0,0), 1),
-  MOTORTRAFIKVEJSTUNNEL(2, new Color(0,0,0), 1),
-  PRIMÆRRUTE(3, new Color(0,0,0), 1),
-  MOTORTRAFIKVEJSAFKØRSEL(3, new Color(0,0,0), 1),
-  PRIMÆRVEJSTUNNEL(3, new Color(0,0,0), 1),
-  SEKUNDÆRRUTE(4, new Color(0,0,0), 1),
-  PROJSEKUNDÆRRUTE(4, new Color(0,0,0),1),
-  PRIMÆRVEJSAFKØRSEL(4, new Color(0,0,0), 1),
-  SEKUNDÆRVEJSTUNNEL(4, new Color(0,0,0), 1),
-  VEJ3(5, new Color(0,0,0), 1), //3-6m
-  ANDENVEJ(5, new Color(0,0,0), 1),
-  PROJVEJ6M(5, new Color(0,0,0), 1), //3-6 m
-  SEKUNDERVEJSAFKØRSEL(5, new Color(0,0,0), 1),
-  ANDENVEJTUNNEL(5, new Color(0,0,0), 1),
-  MARKVEJ(6, new Color(0,0,0), 1),
-  GÅGADER(6, new Color(0,0,0), 1),
-  PROJVEJ3M(6, new Color(0,0,0), 1), //<3m
-  ANDENVEJAFKØRESL(6, new Color(0,0,0), 1),
-  MINDREVEJTUNNEL(6, new Color(0,0,0), 1),
-  STI(7, new Color(0,0,0), 1),
-  PROJSTI(7, new Color(0,0,0), 1),
-  STITUNNEL(7, new Color(0,0,0), 1),
-  UKENDT(7, new Color(0,0,0), 1);
+  HIGHWAY(1, new Color(0,0,0), 1),
+  PROJHIGHWAY(1, new Color(0,0,0), 1),
+  HIGHWAYTUNNEL(1, new Color(0,0,0), 1),
+  FERRY(1, new Color(0,0,0), 1),
+  EXPRESSWAY(2, new Color(0,0,0), 1),
+  PROJEXPRESSWAY(2, new Color(0,0,0), 1),
+  PROJPRIMARYWAY(2, new Color(0,0,0), 1),
+  HIGHWAYINTERSECTION(2, new Color(0,0,0), 1),
+  EXPRESSWAYTUNNEL(2, new Color(0,0,0), 1),
+  PRIMARYWAY(3, new Color(0,0,0), 1),
+  EXPRESSWAYEXIT(3, new Color(0,0,0), 1),
+  PRIMARYWAYTUNNEL(3, new Color(0,0,0), 1),
+  SEKUNDARYWAY(4, new Color(0,0,0), 1),
+  PROJSEKUNDARYWAY(4, new Color(0,0,0),1),
+  PRIMARYWAYINTERSECTION(4, new Color(0,0,0), 1),
+  SEKUNDARYWAYTUNNEL(4, new Color(0,0,0), 1),
+  ROAD3M(5, new Color(0,0,0), 1), //3-6m
+  ANDENVEJ(5, new Color(0,0,0), 1), //PAS PÃ… HVORDAN DEN SKULLE OVERSÃ†TTES
+  PROJROAD6M(5, new Color(0,0,0), 1), //3-6 m
+  SEKUNDARYWAYINTERSECTION(5, new Color(0,0,0), 1),
+  ANDENVEJTUNNEL(5, new Color(0,0,0), 1), //IGEN PAS
+  LANE(6, new Color(0,0,0), 1),
+  PEDESTRIAN(6, new Color(0,0,0), 1),
+  PROJROAD3M(6, new Color(0,0,0), 1), //<3m
+  SEKUNDARYWAYINTERSECTION(6, new Color(0,0,0), 1),
+  SMALLROADTUNNEL(6, new Color(0,0,0), 1),
+  PATH(7, new Color(0,0,0), 1),
+  PROJPATH(7, new Color(0,0,0), 1),
+  PATHTUNNEL(7, new Color(0,0,0), 1),
+  UNKNOWN(7, new Color(0,0,0), 1);
 
-  private final int priotet;
+  private final int priority;
   private final Color color;
   private final double width;
 
   RoadType(int prio, Color color, double width){
-    this.priotet = prio;
+    this.priority = prio;
     this.color = color;
     this.width = width;
   }
 
-  public int priotet(){
-    return priotet;
+  public int priority(){
+    return priority;
   }
   public Color color(){
     return color;
