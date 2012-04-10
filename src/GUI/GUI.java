637cc573ecc196e1e8441b87c86e6b6da8b51235
Anders <afri@itu.dk>
2012-04-10 14:37:01 +0200

    Tilføjet zoom boks

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -41,8 +41,10 @@ public class GUI extends JComponent {
 	private int xClicked, yClicked, xMove, yMove;
 	// The map from the controller
 	private JComponent map;
+	private boolean isMouseDown;
 	
     public GUI() {
+    	Controller.getInstance();
 		makeFrame();
 		makeMenuBar();
 		map = Controller.getMap();
@@ -204,8 +206,10 @@ public class GUI extends JComponent {
 	}
 	
 	public void paint(Graphics g) {
+		if(isMouseDown) {
 		g.setColor(new Color(255,255,255));
 		g.drawRect(xClicked, yClicked, xMove-xClicked, yMove-yClicked);
+		}
 	}
 	
 	private void createZoomRect(JComponent map) {
@@ -213,6 +217,7 @@ public class GUI extends JComponent {
 			public void mousePressed(MouseEvent e) {
 				xClicked = e.getX();
 				yClicked = e.getY();
+				isMouseDown = true;
 				repaint();
 			}
 
@@ -227,6 +232,7 @@ public class GUI extends JComponent {
 				yClicked = -10;
 				xMove = -10;
 				yMove = -10;
+				isMouseDown = false;
 				repaint();
 			} 
 		});
