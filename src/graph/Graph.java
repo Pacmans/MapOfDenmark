dd392c6b66205710a398d559be7e45236b819dea
Claus L. Henriksen <clih@itu.dk>
2012-04-19 10:50:08 +0200

    Graph class

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,12 +1,7 @@
 package graph;
 
-import java.util.HashSet;
-import java.util.Iterator;
-
 import controller.Controller;
 import dataStructure.Connection;
-import dataStructure.ConnectionQuadTree;
-import dataStructure.Node;
 import dataStructure.Point;
 
 /**Graph handler class for points. 
@@ -35,24 +30,8 @@ public class Graph {
     g = new EdgeWeightedDigraph(points.length, connections.length); //vertices, edges 
     
     //add edges
-    
-    
-    
-    
-    Iterator<Node> it = Controller.getInstance().getQT().iterator();
-    while(it.hasNext()){
-      Node n = it.next();
-      for(Integer i : n.getConnections()){
-        Connection c = connections[i];
-        //add edge to it self
-        g.addEdge(new DirectedEdge(c.getID(), c.getID(), c.getWeight()));
-        
-        //add other vertex
-        Connection other;
-        
-        g.addEdge(new DirectedEdge(v, w, con.getWeight()));
-      }
-      
+    for(Connection c : connections){
+      g.addEdge(new DirectedEdge(c.getLeft().getID(), c.getRight().getID(), c.getWeight()));
     }
   }
 }
