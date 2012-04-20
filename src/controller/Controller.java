3e8cdd680d48b105a9124977ab4f2d0cb2819b36
bhas <bhas@itu.dk>
2012-04-20 19:33:37 +0300

    Ryddet op i Controller og ændret så kun instance er static og resten er normalt. Alle klasser skal hente controlleren som instans først

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,9 +6,6 @@ import files.FileLoaderConnectionOnly;
 import gui.GUI;
 
 import java.util.HashSet;
-
-import javax.swing.JComponent;
-
 import visualization.MapComponent;
 import dataStructure.Connection;
 import dataStructure.ConnectionQuadTree;
@@ -22,163 +19,160 @@ import dataStructure.Interval2D;
  * @version 10. April 2012
  * 
  */
-public final class Controller extends Thread {
-  private static Controller instance; // singleton
-  private static GUI gui; // singleton
-  private static MapComponent map; // singleton
-  private static FileLoader fileLoader;
-  private Connection[] connections;
-  private ConnectionQuadTree qt;
-  private static double xMin, yMin, xMax, yMax;
+public final class Controller {
+	private static Controller instance; // singleton
+	private GUI gui;
+	private MapComponent map;
+	private FileLoader fileLoader;
+	private Connection[] connections;
+	private ConnectionQuadTree qt;
+	private double xMin, yMin, xMax, yMax;
 
-/**
-   * Constructor for this class loads connections and points from FileLoader
-   * 
-   * @see FileLoader
-   */
-  public Controller() {
-    if (instance == null)
-      instance = this;
-    try {
-      fileLoader = new FileLoaderConnectionOnly();
-      connections = fileLoader.getConnections();
-      xMin = fileLoader.getxMin().intValue();
-      yMin = fileLoader.getyMin().intValue();
-      xMax = fileLoader.getxMax().intValue();
-      yMax = fileLoader.getyMax().intValue();
-      qt = fileLoader.getConnectionQuadTree();
-    } catch (Exception e) {
-    	showAlert(e);
-    }
-    fileLoader = null;
-  }
-  
-  /**
-   * 
-   * @return Returns an instance of the singleton Controller (this class)
-   */
-  public static Controller getInstance() {
-    if (instance == null)
-      return new Controller(); // should not happen
-    return instance;
-  }
+	/**
+	 * Constructor for this class loads connections and points from FileLoader
+	 * 
+	 * @see FileLoader
+	 */
+	public Controller() {
+		createInstances();
+		try {
+			fileLoader = new FileLoaderConnectionOnly();
+			connections = fileLoader.getConnections();
+			xMin = fileLoader.getxMin().intValue();
+			yMin = fileLoader.getyMin().intValue();
+			xMax = fileLoader.getxMax().intValue();
+			yMax = fileLoader.getyMax().intValue();
+			qt = fileLoader.getConnectionQuadTree();
+		} catch (Exception e) {
+			ExceptionController.recieveException(e);
+		}
+		fileLoader = null;
+	}
 
-  /**
-   * 
-   * @return Returns instance of the singleton class GUI
-   * @see GUI-class
-   */
-  public static GUI getGUI() {
-    if (gui == null)
-      gui = new GUI();
-    return gui;
-  }
+	/**
+	 * creates one singleton controller and initialize gui and map. Once one
+	 * Controller instance is made you can't get another Controller. Other
+	 * classes should always get map and gui from the controller
+	 */
+	private void createInstances() {
+		if (instance == null) {
+			instance = this;
+			gui = new GUI();
+			map = new MapComponent();
+		}
+	}
 
-  /**
-   * 
-   * @return Returns instance of the singleton class Map which paints the map
-   * @see MapImpl
-   */
-  public static JComponent getMap() {
-    if (map == null)
-      map = new MapComponent();
-    return map;
-  }
+	/**
+	 * 
+	 * @return Returns an instance of the singleton Controller (this class)
+	 */
+	public static Controller getInstance() {
+		if (instance == null) // should not happen
+			new Controller(); //Contructor in Controller updates instance
+		return instance;
+	}
 
-  /**
-   * Get all connections within rectangle
-   * 
-   * @param x1
-   * @param y1
-   * @param x2
-   * @param y2
-   *          ArrayList of connections within rectangle
-   * @return
-   */
-  public Connection[] getConnections(double x1, double y1, double x2, double y2){
-    System.out.println(x1+" "+y1+" "+x2+" "+y2);
-    //get HashSet of connection IDs from QuadTree
-    //this is fast?
-    HashSet<Integer> cons = qt.getConnections(new Interval2D(new Interval(x1, x2), new Interval(y1, y2)));
-    Connection[] cs = new Connection[cons.size()];
-    int size = 0;
-    for(Integer i : cons){ //this is slow
-      cs[size++] = connections[i];
-    }
-    return cs;
-  }
+	/**
+	 * 
+	 * @return Returns instance of the singleton class GUI
+	 * @see GUI
+	 */
+	public GUI getGUI() {
+		return gui;
+	}
 
-  /**
-   * Get array of all connections
-   * 
-   * @return Array of all connections
-   * @see Connection
-   */
-  public Connection[] getConnections() {
-    return connections;
-  }
-  
-  /**
-   * Show alert on GUI 
-   * @param s Alert to be shown
-   */
-  public static void showAlert(Exception e){
-	ExceptionController.recieveException(e);
-  }
-  
-  
-  /**
-   * Set status label on GUI
-   * @param s Status to be shown
-   */
-  public static void setStatus(String s){
-//    GUI.setStatus(s);
-  }
+	/**
+	 * 
+	 * @return Returns instance of the singleton class Map which paints the map
+	 * @see MapComponent
+	 */
+	public MapComponent getMap() {
+		return map;
+	}
 
-  /**
-   * Show or hide a type of road
-   * 
-   * @param n
-   *          type_id
-   * @param b
-   *          To show or not to show
-   */
-  public static void updateMap(int n, boolean m) {
-	  UpdateMapThread u = new UpdateMapThread(n,m, map);
-	  Thread uPMT = new Thread(u);
-	  uPMT.start();
-  }
-//  public static void scaleMap(int i, int j) {
-//      map.setScale();
-//  }
-  
+	/**
+	 * Get all connections within rectangle
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 *            ArrayList of connections within rectangle
+	 * @return
+	 */
+	public Connection[] getConnections(double x1, double y1, double x2,
+			double y2) {
+		// get HashSet of connection IDs from QuadTree
+		// this is fast? NOOOOOOO!
+		HashSet<Integer> cons = qt.getConnections(
+				new Interval2D(new Interval(x1, x2), new Interval(y1, y2)));
+		Connection[] cs = new Connection[cons.size()];
+		int size = 0;
+		for (Integer i : cons) { // this is slow
+			cs[size++] = connections[i];
+		}
+		return cs;
+	}
 
-public static void showAll(){
-    map.resetZoom();
-  }
+	/**
+	 * Get array of all connections
+	 * 
+	 * @return Array of all connections
+	 * @see Connection
+	 */
+	public Connection[] getConnections() {
+		return connections;
+	}
 
-  public static double getxMin() {
-    return xMin;
-  }
+	/**
+	 * Set status label on GUI
+	 * 
+	 * @param s
+	 *            Status to be shown
+	 */
+	public static void setStatus(String s) {
+		// GUI.setStatus(s);
+	}
 
-  public static double getyMin() {
-    return yMin;
-  }
+	/**
+	 * Show or hide a type of road
+	 * 
+	 * @param n
+	 *            type_id
+	 * @param b
+	 *            To show or not to show
+	 */
+	public void updateMap(int n, boolean m) {
+		map.updateRoadTypes(n, m);
+	}
 
-  public static double getxMax() {
-    return xMax;
-  }
+	public void showAll() {
+		map.resetZoom();
+	}
 
-  public static double getyMax() {
-    return yMax;
-  }
-  /**
-   * Main method creates a new GUI
-   * 
-   * @see GUI-class
-   * @param args
-   */
-  public static void main(String[] args) {
-    new GUI();
-  }
+	public double getxMin() {
+		return xMin;
+	}
+
+	public double getyMin() {
+		return yMin;
+	}
+
+	public double getxMax() {
+		return xMax;
+	}
+
+	public double getyMax() {
+		return yMax;
+	}
+
+	/**
+	 * Main method creates a new GUI
+	 * 
+	 * @see GUI-class
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Controller();
+	}
 }
