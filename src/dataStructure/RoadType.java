8ae5cedfc8536ad54a6a9f6cb76e8887be5d2379
bhas <bhas@itu.dk>
2012-04-26 03:19:44 +0300

    nye farver

--- src/dataStructure/RoadType.java
+++ src/dataStructure/RoadType.java
@@ -18,36 +18,36 @@ import java.awt.Color;
  *
  */
 public enum RoadType {
-  HIGHWAY(1, new Color(250,0,0), 1),
-  PROJHIGHWAY(1, new Color(250,0,0), 1),
-  HIGHWAYTUNNEL(1, new Color(250,0,0), 1),
-  EXPRESSWAY(2, new Color(218,165,32), 1),
-  PROJEXPRESSWAY(2, new Color(218,165,32), 1),
-  PROJPRIMARYWAY(2, new Color(218,165,32), 1),
-  HIGHWAYINTERSECTION(2, new Color(218,165,32), 1),
-  EXPRESSWAYTUNNEL(2, new Color(218,165,32), 1),
-  PRIMARYWAY(3, new Color(222,184,135), 1),
-  EXPRESSWAYEXIT(3, new Color(222,184,135), 1),
-  PRIMARYWAYTUNNEL(3, new Color(222,184,135), 1),
-  SECONDARYWAY(4, new Color(222,184,135), 1),
-  PROJSECONDARYWAY(4, new Color(222,184,135),1),
-  PRIMARYWAYINTERSECTION(4, new Color(222,184,135), 1),
-  SECONDARYWAYTUNNEL(4, new Color(222,184,135), 1),
-  ROAD3M(5, new Color(193,205,193), 1), //3-6m
-  OTHERROAD(5, new Color(193,205,193), 1), //PAS PÅ HVORDAN DEN SKULLE OVERSÆTTES
-  PROJROAD6M(5, new Color(193,205,193), 1), //3-6 m
-  SECONDARYWAYINTERSECTION(5, new Color(193,205,193), 1),
-  OTHERROADTUNNEL(5, new Color(193,205,193), 1), // ANDEN VEJ
-  FERRY(5, new Color(139,137,137), 1),
-  LANE(6, new Color(34,139,34), 1),
-  PEDESTRIAN(6, new Color(211,211,211), 1),
-  PROJROAD3M(6, new Color(211,211,211), 1), //<3m
-  OTHERROADINTERSECTION(6, new Color(211,211,211), 1), // ANDEN VEJ
-  SMALLROADTUNNEL(6, new Color(211,211,211), 1),
-  PATH(7, new Color(34,139,34), 1),
-  PROJPATH(7, new Color(34,139,34), 1),
-  PATHTUNNEL(7, new Color(34,139,34), 1),
-  UNKNOWN(7, new Color(211,211,211), 1);
+  HIGHWAY(1, Color.red, 1),
+  PROJHIGHWAY(1, Color.red, 1),
+  HIGHWAYTUNNEL(1, Color.red, 1),
+  EXPRESSWAY(2, Color.orange, 1),
+  PROJEXPRESSWAY(2, Color.orange, 1),
+  PROJPRIMARYWAY(2, Color.orange, 1),
+  HIGHWAYINTERSECTION(2, Color.orange, 1),
+  EXPRESSWAYTUNNEL(2, Color.orange, 1),
+  PRIMARYWAY(3, new Color(50,50,50), 1),
+  EXPRESSWAYEXIT(3, new Color(50,50,50), 1),
+  PRIMARYWAYTUNNEL(3, new Color(50,50,50), 1),
+  SECONDARYWAY(4, new Color(125,125,125), 1),
+  PROJSECONDARYWAY(4, new Color(125,125,125),1),
+  PRIMARYWAYINTERSECTION(4, new Color(125,125,125), 1),
+  SECONDARYWAYTUNNEL(4, new Color(125,125,125), 1),
+  ROAD3M(5, new Color(200,200,200), 1), //3-6m
+  OTHERROAD(5, new Color(200,200,200), 1), //PAS PÃ… HVORDAN DEN SKULLE OVERSÃ†TTES
+  PROJROAD6M(5, new Color(200,200,200), 1), //3-6 m
+  SECONDARYWAYINTERSECTION(5, new Color(200,200,200), 1),
+  OTHERROADTUNNEL(5, new Color(200,200,200), 1), // ANDEN VEJ
+  FERRY(5, new Color(200,200,200), 1),
+  LANE(6, Color.blue, 1),
+  PEDESTRIAN(6, Color.blue, 1),
+  PROJROAD3M(6, Color.blue, 1), //<3m
+  OTHERROADINTERSECTION(6, Color.blue, 1), // ANDEN VEJ
+  SMALLROADTUNNEL(6, Color.blue, 1),
+  PATH(7, Color.green, 1),
+  PROJPATH(7, Color.green, 1),
+  PATHTUNNEL(7, Color.green, 1),
+  UNKNOWN(7, Color.green, 1);
 
   private final int priority;
   private final Color color;
