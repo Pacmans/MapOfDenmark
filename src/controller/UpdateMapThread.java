143438359ccfa2257b73e9d8ebe60c5ead993857
Drescher86 <mdre@itu.dk>
2012-05-07 11:44:55 +0200

    merge

--- src/controller/UpdateMapThread.java
+++ src/controller/UpdateMapThread.java
@@ -2,16 +2,20 @@ package controller;
 
 import visualization.MapComponent;
 
+
 public class UpdateMapThread implements Runnable {
 private int n;
 private boolean m;
 private static MapComponent map;
 	
+
 	public UpdateMapThread(int n, boolean m, MapComponent map) {
 		this.n = n;
 		this.m = m;
 		UpdateMapThread.map = map;
 	}
+	
+
 	@Override
 	public void run() {
 		synchronized(map) {
