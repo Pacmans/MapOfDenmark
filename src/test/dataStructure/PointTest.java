74f84662fce413417f01b108cc71e39c0b7a6089
Claus L. Henriksen <clih@itu.dk>
2012-05-15 12:00:51 +0200

    asdf

---
+++ src/test/dataStructure/PointTest.java
@@ -0,0 +1,51 @@
+package test.dataStructure;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dataStructure.Point;
+
+public class PointTest {
+  Point instance = new Point(1, 2, 3); //id, x, y
+
+  @Test
+  public void testGetX() {
+    assertEquals(instance.getX(), 2, 0.1);
+  }
+
+  @Test
+  public void testGetY() {
+    assertEquals(instance.getY(), 3, 0.1);
+  }
+
+  @Test
+  public void testGetID() {
+    assertEquals(instance.getID(), 1, 0.1);
+  }
+
+  @Test
+  public void testGetZip() {
+    int test = 2300;
+    instance.setZip(test);
+    assertEquals(test, instance.getZip());
+  }
+
+  @Test
+  public void testCompareTo() {
+    Point test = new Point(2, 3, 4);
+    assert(instance.compareTo(test) == -1);
+  }
+  
+  @Test
+  public void testCompareTo2() {
+    Point test = new Point(0, 3, 4);
+    assert(instance.compareTo(test) == 1);
+  }
+  
+  @Test
+  public void testCompareTo3() {
+    Point test = new Point(2, 3, 4);
+    assert(instance.compareTo(test) == 0);
+  }
+}
